5af24d29d37d17da560c9e13850997c56e9edb2b
Wulfre <6633817+Wulfre@users.noreply.github.com>
2023-10-28 01:59:28 -0400

    enhance gitignore parsing

--- src/configs/ignores.js
+++ src/configs/ignores.js
@@ -1,9 +1,17 @@
-import { readFileSync } from "node:fs"
+import { readFileSync, existsSync } from "node:fs"
+import parse from "parse-gitignore"
 
-const gitignores = readFileSync(".gitignore", "utf8").replaceAll(/#.*/g, "").split(/\r?\n/).filter(Boolean)
+const gitignoreFile = ".gitignore"
+const gitignoreGlobs = existsSync(gitignoreFile)
+    ? parse(readFileSync(gitignoreFile, "utf8")).globs().reduce((acc, glob) => (
+        glob.type === "ignore"
+            ? [...acc, ...glob.patterns]
+            : [...acc, ...glob.patterns.map((pattern) => `!${pattern}`)]
+    ), [])
+    : []
 
 export const ignoresConfig = [
     {
-        ignores: [...gitignores],
+        ignores: [...gitignoreGlobs],
     },
 ]
