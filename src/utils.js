d87c4f880936ced9d109e9be4e918d715be9c526
Wulfre <6633817+Wulfre@users.noreply.github.com>
2023-11-04 11:45:46 -0400

    refactor utils using new eslint rules, bump version

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,13 @@ import fg from "fast-glob"
 export const hasTypescript = fg.globSync("**/tsconfig.json").length > 0
 export const hasUnocss = fg.globSync("**/unocss.config.?([cm])[jt]s").length > 0
 
-export const renameConfigRules = (rules, prefix) => Object.fromEntries(
-    Object.entries(rules).map(([key, value]) => {
-        const newKey = key.includes("/")
-            ? `${prefix}/${key.split("/").slice(1).join("/")}`
-            : key
-        return [newKey, value]
-    }),
-)
+export function renameConfigRules(rules, prefix) {
+    return Object.fromEntries(
+        Object.entries(rules).map(([key, value]) => {
+            const newKey = key.includes("/")
+                ? `${prefix}/${key.split("/").slice(1).join("/")}`
+                : key
+            return [newKey, value]
+        }),
+    )
+}
